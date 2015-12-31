6d915d33143c09346af621c00c88c4932190f81c
airtoxin <airtoxin@icloud.com>
2016-01-01 07:53:47 +0900

    Add canvas template

--- src/ipython_array_animation.py
+++ src/ipython_array_animation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 from IPython.display import HTML, display
+from jinja2 import Environment, FileSystemLoader
 
 class AnimateArray(object):
     def __init__(self):
         super(AnimateArray, self).__init__()
+        self.env = Environment(loader=FileSystemLoader('./template/', encoding='utf8'))
 
     def show(self):
-        display(HTML('<h1>hello</h1>'))
+        template = self.env.get_template('animatetag.tpl.html')
+        display(HTML(template.render()))
