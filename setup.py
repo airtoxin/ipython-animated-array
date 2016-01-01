7e7ffd96179fc33e1bd325e1e25c3cf973301c0e
airtoxin <airtoxin@icloud.com>
2016-01-01 11:41:50 +0900

    Fix setupscript

--- setup.py
+++ setup.py
@@ -8,13 +8,25 @@ with open('README.rst') as f:
 setup(
     name='ipython-array-animation',
     version='0.0.1',
-    description='',
+    description='animated array rendering on ipython-notebook (jupyter)',
     long_description=long_description,
     author='Ryoji Ishii',
     author_email='airtoxin@icloud.com',
     url='https://github.com/airtoxin/ipython-array-animation',
     packages=['ipython-array-animation'],
     package_dir={'ipython-array-animation': 'src'}
-    classifiers=[],
-    install_requires=[]
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Framework :: IPython',
+        'Topic :: Scientific/Engineering :: Visualization'
+    ],
+    install_requires=[
+        'jinja2'
+        'vizarray',
+        'ipythonblocks', # vizarray dependency
+        'numpy', # vizarray dependency
+        'matplotlib<1.5', # vizarray dependency
+    ]
 )
